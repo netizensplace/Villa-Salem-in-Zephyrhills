2b974c1ced1a548aef382214dd6029ad5c07efbc
hardhouses <125613125+hardhouses@users.noreply.github.com>
2023-03-22 18:51:00 -0600

    Add additional files

---
+++ assets/js/main.js
@@ -0,0 +1,292 @@
+(function () {
+  'use strict';
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener);
+  };
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header');
+    let offset = header.offsetHeight;
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16;
+    }
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth',
+    });
+  };
+
+  /**
+   * Header fixed top on scroll
+   */
+  let selectHeader = select('#header');
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop;
+    let nextElement = selectHeader.nextElementSibling;
+    const headerFixed = () => {
+      if (headerOffset - window.scrollY <= 0) {
+        selectHeader.classList.add('fixed-top');
+        nextElement.classList.add('scrolled-offset');
+      } else {
+        selectHeader.classList.remove('fixed-top');
+        nextElement.classList.remove('scrolled-offset');
+      }
+    };
+    window.addEventListener('load', headerFixed);
+    onscroll(document, headerFixed);
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top');
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active');
+      } else {
+        backtotop.classList.remove('active');
+      }
+    };
+    window.addEventListener('load', toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile');
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    'click',
+    '.scrollto',
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
+
+        let navbar = select('#navbar');
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile');
+          let navbarToggle = select('.mobile-nav-toggle');
+          navbarToggle.classList.toggle('bi-list');
+          navbarToggle.classList.toggle('bi-x');
+        }
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select('#hero-carousel-indicators');
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true);
+
+  heroCarouselItems.forEach((item, index) => {
+    index === 0
+      ? (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
+          index +
+          "' class='active'></li>")
+      : (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
+          index +
+          "'></li>");
+  });
+
+  /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120,
+      },
+    },
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows',
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on(
+        'click',
+        '#portfolio-flters li',
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove('filter-active');
+          });
+          this.classList.add('filter-active');
+
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter'),
+          });
+        },
+        true
+      );
+    }
+  });
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox',
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  });
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+})();
+
+// Pagination for attractions and restaurants
+const container = document.getElementById('blog');
+const posts = container.querySelectorAll('.post');
+const postsPerPage = 5;
+const totalpages = Math.ceil(posts.length / postsPerPage);
+
+let currentPage = 1;
+
+// Function to update the displayed posts based on the current page number
+function updatePosts() {
+  const startIndex = (currentPage - 1) * postsPerPage;
+  const endIndex = startIndex + postsPerPage;
+
+  for (let i = 0; i < posts.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      posts[i].style.display = 'block';
+    } else {
+      posts[i].style.display = 'none';
+    }
+  }
+
+  const pageNumber = document.getElementById('page-number');
+  pageNumber.textContent = `Page ${currentPage} of ${totalpages}`;
+}
+// Add event listeners for the pagination buttons
+const prevLink = document.getElementById('prev-page');
+prevLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (currentPage > 1) {
+    currentPage--;
+    updatePosts();
+  }
+});
+const nextLink = document.getElementById('next-page');
+nextLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (currentPage < totalpages) {
+    currentPage++;
+    updatePosts();
+  }
+});
+// Display the initial page of posts
+updatePosts();
